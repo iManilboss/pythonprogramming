acd4fca82a01e25e513caec63aba552316352fd2
Anil Bikram Thapa <imaniboss@gmail.com>
2023-08-15 21:09:44 +0545

    upto numeric type done

---
+++ Datatype.py
@@ -0,0 +1,27 @@
+#Numeric Type: int, float, complex
+x = int(20)
+print("Integer number example ",+x)
+
+y= float(13.4)
+print("Float number example:",+y)
+
+z = complex(1j)
+print("Complex number is like: ",+z)
+
+#String
+
+
+#Sequence Type: list, tuple, range
+
+
+
+#Mapping Type: dict
+
+
+
+#Set Types: set, frozenset
+
+
+
+#Boolean Type: bool
+
