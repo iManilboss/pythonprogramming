edabe0feab83d8cd71a887a3e3a8d1724d39217d
Anil Bikram Thapa <imaniboss@gmail.com>
2023-08-15 20:02:38 +0545

    variables and its properties

--- Tutor1.py
+++ Tutor1.py
@@ -1,1 +1,22 @@
 print("Hello, World!")
+#variables
+x = 1 #it is a type of int
+y = "anil" #it is of type str
+print(x)
+print(y)
+print(type(x))
+print(type(y))
+
+#typecasting
+x= str(3)
+print("new type of x is:")
+print(type(x))
+
+
+#case sensitive
+a = 4
+A = "anil"
+#here A don't overwrite the value of a
+print("The examples of case-sensitive are:")
+print(a)
+print(A)
