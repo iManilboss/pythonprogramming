e3c43624ebb0224731b3de9d1c4090bdc5a6dc23
Anil Bikram Thapa <imaniboss@gmail.com>
2023-08-18 13:04:04 +0545

    python string updated

--- Datatype.py
+++ Datatype.py
@@ -9,7 +9,29 @@ z = complex(1j)
 print("Complex number is like: ",+z)
 
 #String
+#an example of a string
+a = "hello world"
+print(a)
 
+#multiline string: we can add multiline string by placing multiple words inside """" """
+multiline = """What is your name?
+            I am Anil Bikram Thapa.
+            I am 25 years old.
+            """
+print(multiline)
+
+#we can loop through the letters as well
+for x in "banana":
+    print(x)
+
+#we can check the string as well.
+txt = "My name is Anil Bikram Thapa"
+if "Anill" in txt:
+    print("Yes, Anil is present in the text.")
+elif "Sagar" not in txt:
+    print("No, Sagar is not in the text.")
+
+#string methods
 
 #Sequence Type: list, tuple, range
 
